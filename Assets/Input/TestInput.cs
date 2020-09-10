cf77c52cbd60bda246bea27cca264b154d81b5f3
unknown <zhanpeng.xie@gtadata.com>
2020-09-10 10:53:09 +0800

    多场景测试

---
+++ Assets/Input/TestInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TestInput : MonoBehaviour
+{
+    public Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    Transform model;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            model = tr;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float X = Input.GetAxis("Mouse X");
+            float Y = Input.GetAxis("Mouse Y");
+                     
+            tr.Rotate(transform.TransformDirection(new Vector3(Y*10,-X*10,0)),Space.World);           
+          
+        }      
+
+        if (model != null)
+        {
+            float axis = Input.GetAxis("Mouse ScrollWheel");
+            if (axis!=0)
+            {
+                model.localScale += Vector3.one * axis * 10; 
+                if (model.localScale.x<0)
+                {
+                    model.localScale = Vector3.zero;
+                }
+            }           
+            Debug.Log(" " + axis);
+        }
+    }
+}
