62539e17ccb43c640a203ae611ac3c0c171067ba
xzp <3094997376@qq.com>
2022-02-19 11:11:40 +0800

    videoPlayer 播放

---
+++ Assets/Graphic/Camera/DynamicResolutionTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicResolutionTest : MonoBehaviour
+{
+    public Text screenText;
+
+    FrameTiming[] frameTimings = new FrameTiming[3];
+
+    public float maxResolutionWidthScale = 1.0f;
+    public float maxResolutionHeightScale = 1.0f;
+    public float minResolutionWidthScale = 0.5f;
+    public float minResolutionHeightScale = 0.5f;
+    public float scaleWidthIncrement = 0.1f;
+    public float scaleHeightIncrement = 0.1f;
+
+    float m_widthScale = 1.0f;
+    float m_heightScale = 1.0f;
+
+    // 跨帧保持的动态分辨率算法变量
+    uint m_frameCount = 0;
+
+    const uint kNumFrameTimings = 2;
+
+    double m_gpuFrameTime;
+    double m_cpuFrameTime;
+
+    // 使用此函数进行初始化
+    void Start()
+    {
+        int rezWidth = (int)Mathf.Ceil(ScalableBufferManager.widthScaleFactor * Screen.currentResolution.width);
+        int rezHeight = (int)Mathf.Ceil(ScalableBufferManager.heightScaleFactor * Screen.currentResolution.height);
+        screenText.text = string.Format("Scale: {0:F3}x{1:F3}\nResolution: {2}x{3}\n",
+            m_widthScale,
+            m_heightScale,
+            rezWidth,
+            rezHeight);
+    }
+
+    // 每帧调用一次 Update
+    void Update()
+    {
+        float oldWidthScale = m_widthScale;
+        float oldHeightScale = m_heightScale;
+
+        // 一根手指降低分辨率
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_heightScale = Mathf.Max(minResolutionHeightScale, m_heightScale - scaleHeightIncrement);
+            m_widthScale = Mathf.Max(minResolutionWidthScale, m_widthScale - scaleWidthIncrement);
+        }
+
+        // 两根手指提高分辨率
+        if (Input.GetButtonDown("Fire2"))
+        {
+            m_heightScale = Mathf.Min(maxResolutionHeightScale, m_heightScale + scaleHeightIncrement);
+            m_widthScale = Mathf.Min(maxResolutionWidthScale, m_widthScale + scaleWidthIncrement);
+        }
+
+        if (m_widthScale != oldWidthScale || m_heightScale != oldHeightScale)
+        {
+            ScalableBufferManager.ResizeBuffers(m_widthScale, m_heightScale);
+        }
+        DetermineResolution();
+        int rezWidth = (int)Mathf.Ceil(ScalableBufferManager.widthScaleFactor * Screen.currentResolution.width);
+        int rezHeight = (int)Mathf.Ceil(ScalableBufferManager.heightScaleFactor * Screen.currentResolution.height);
+        screenText.text = string.Format("Scale: {0:F3}x{1:F3}\nResolution: {2}x{3}\nScaleFactor: {4:F3}x{5:F3}\nGPU: {6:F3} CPU: {7:F3}",
+            m_widthScale,
+            m_heightScale,
+            rezWidth,
+            rezHeight,
+            ScalableBufferManager.widthScaleFactor,
+            ScalableBufferManager.heightScaleFactor,
+            m_gpuFrameTime,
+            m_cpuFrameTime);
+    }
+
+    // 估算下一帧时间并在必要时更新分辨率缩放。
+    private void DetermineResolution()
+    {
+        ++m_frameCount;
+        if (m_frameCount <= kNumFrameTimings)
+        {
+            return;
+        }
+        FrameTimingManager.CaptureFrameTimings();
+        FrameTimingManager.GetLatestTimings(kNumFrameTimings, frameTimings);
+        if (frameTimings.Length < kNumFrameTimings)
+        {
+            Debug.LogFormat("Skipping frame {0}, didn't get enough frame timings.",
+                m_frameCount);
+
+            return;
+        }
+
+        m_gpuFrameTime = (double)frameTimings[0].gpuFrameTime;
+        m_cpuFrameTime = (double)frameTimings[0].cpuFrameTime;
+    }
+}
---
+++ Assets/Video/Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Test : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    public Slider slider;
+
+    private bool isMouseDown = false;
+
+    private void OnEnable()
+    {
+        isMouseDown = false;
+        videoPlayer.frame = 0;
+        videoPlayer.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
+        //videoPlayer.frame = 2000;
+        Debug.Log(videoPlayer.length);
+        slider.onValueChanged.AddListener((process) =>
+        {
+            if (isMouseDown)
+            {
+                double curFrame = videoPlayer.length * slider.value * videoPlayer.frameRate;
+                videoPlayer.frame = System.Convert.ToInt32(curFrame);
+                //videoPlayer.Play();
+            }
+        });
+        
+    }
+
+    private void Update()
+    {
+        if (!isMouseDown)
+        {
+            slider.value = (float)videoPlayer.frame / videoPlayer.frameCount;
+        }
+    }
+
+    private void VideoPlayer_loopPointReached(VideoPlayer source)
+    {
+       Debug.Log(videoPlayer.length);
+    }
+
+    public void OnPointerDown()
+    {
+        isMouseDown = true;
+    }
+
+
+    public void OnPointerUp()
+    {
+        isMouseDown = false;
+    }
+    
+   
+}
