d38cf3a1a1d01d44402a4d128187018212fa2ab2
xzp <3094997376@qq.com>
2022-03-02 15:55:39 +0800

    地形功能开发

---
+++ Assets/Terrian/TestTerrian.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTerrian : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
